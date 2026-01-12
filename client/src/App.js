8ddec6b5aedf6b5152360cee7559d00bf23dbfd5
Rahul kumar singh <rahulkumarsingh@Rahuls-MacBook-Air.local>
2026-01-12 16:15:58 +0530

    add navbar button and creating pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,44 @@
 import styled, { ThemeProvider } from "styled-components";
 import { darkTheme } from "./utils/Theam"
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import NavBar from "./components/NavBar";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`
+
+const Wrapper = styled.div`
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column; 
+  justify-content: space-between;
+  flex: 3;
+`;
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-      Hello
+      <Container>
+        <Wrapper>
+          <BrowserRouter>
+          <NavBar/>
+          <Routes>
+            <Route path="/" element={<Home/>} exact/>
+            <Route path="/create-post" element={<CreatePost/>} exact/>
+          </Routes>
+          </BrowserRouter>
+        </Wrapper>
+      </Container>
     </ThemeProvider>
   );
 }
